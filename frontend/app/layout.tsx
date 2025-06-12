9109b1079a3dbd60b018cf2733a59a2b98939da5
Manujdixit <manujdixit04@gmail.com>
2025-06-12 09:51:53 +0530

    folder restructure

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
-import ArticlesSection from "@/components/articles";
-import CostComparisonSection from "@/components/costComparison";
-import HeroSection from "@/components/Hero";
-import ProcessSection from "@/components/process";
-import TestimonialsSection from "@/components/testimonials";
-import UniversitiesSection from "@/components/universities";
+import ArticlesSection from "@/components/sections/articles";
+import CostComparisonSection from "@/components/sections/costComparison";
+import HeroSection from "@/components/sections/Hero";
+import ProcessSection from "@/components/sections/process";
+import TestimonialsSection from "@/components/sections/testimonials";
+import UniversitiesSection from "@/components/sections/universities";
 
 export default function Home() {
   return (
